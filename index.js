e5070392ef13c934a252bac257eb3c78814e4a2b
liambickett <liam@arc-creative.co>
2023-02-16 15:54:50 -0800

    initial commit

---
+++ index.js
@@ -0,0 +1,11 @@
+import express from 'express';
+
+const app = express();
+
+app.get('/', (req, res) => {
+  res.send({ hi: 'there' });
+});
+
+const PORT = process.env.PORT;
+
+app.listen(PORT || 5000);
