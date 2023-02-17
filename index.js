c92e7b9c215b107dc859c5a4fb98c3b38f64c676
liambickett <liam@arc-creative.co>
2023-02-16 16:39:34 -0800

    updated string

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ import express from 'express';
 const app = express();
 
 app.get('/', (req, res) => {
-  res.send({ hi: 'there' });
+  res.send({ hi: 'nerds' });
 });
 
 const PORT = process.env.PORT;
