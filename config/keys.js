1ef3392719f9917e1f371d5fcfdad1714b31bdf0
liambickett <liam@arc-creative.co>
2023-02-21 19:11:01 -0800

    bugfixing

--- config/keys.js
+++ config/keys.js
@@ -1,6 +1,3 @@
-// import { cookieKeysDev, googleKeysDev, mongoURIDev } from './dev.js';
-// import { cookieKey, googleKeys, mongoURI } from './prod.js';
-
 export function useKeys() {
   if (process.env.NODE_ENV === 'production') {
     return {
@@ -13,9 +10,14 @@ export function useKeys() {
     };
   } else {
     return {
-      // googleKeys: googleKeysDev,
-      // mongoURI: mongoURIDev,
-      // cookieKey: cookieKeysDev,
+      googleKeys: {
+        client_id:
+          '915666666210-ekebrkgpc79k1pp359is38333ltd24p6.apps.googleusercontent.com',
+        client_secret: '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',
+      },
+      mongoURI:
+        'mongodb+srv://server:pass@betabase.sm8furz.mongodb.net/?retryWrites=true&w=majority',
+      cookieKey: 'k1k3jh128H@uh3123k%R2j123bnS9d3u91jsad92!ASDF3@',
     };
   }
 }
